bdc27940ecae0da4838bc4b6afbf8aef28213528
Matheus Gomes <matheus_mgc@yahoo.com.br>
2024-12-30 00:00:37 -0300

    add skills section

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,14 +1,46 @@
 import { SectionTitle } from "../sectionTitle/section-title";
+import Image from "next/image";
 import "./experience.scss";
 
 export function Experience() {
-    return (
+  return (
     <div className="experience">
-        <SectionTitle text="Experience"/>
-        <p>2 years as an intern in systems analysis.</p>
-        <div className="experience-time">
-
+      <SectionTitle text="Experience" />
+      <p>2 years as an intern in systems analysis.</p>
+      <div className="experience-time">
+        <div className="experience-language">
+          <Image src="/react.png" alt="React logo" width={40} height={40} />
+          <div className="experience-unit">
+            <div className="experience-measure measure-2">
+                <span>2 years</span>
+            </div>
+          </div>
         </div>
+        <div className="experience-language">
+          <Image src="/ts.png" alt="TypeScript logo" width={40} height={40} />
+          <div className="experience-unit">
+            <div className="experience-measure measure-3">
+                <span>3 years</span>
+            </div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image src="/js.png" alt="JavaScript logo" width={40} height={40} />
+          <div className="experience-unit">
+            <div className="experience-measure measure-3">
+                <span>3 years</span>
+            </div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image src="/java.png" alt="Java logo" width={40} height={40} />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">
+                <span>1 years</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
