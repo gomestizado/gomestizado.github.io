d36637c7ea1e4c40f84fda265b4e18d2195af95e
Matheus Gomes <matheus_mgc@yahoo.com.br>
2025-01-02 00:55:01 -0300

    update informations

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -6,24 +6,24 @@ export function Experience() {
   return (
     <div className="experience">
       <div className="experience-header">
-        <SectionTitle text="Experience" />
-        <p>2 years as an intern in systems analysis.</p>
+        <SectionTitle text="Experience 💻" />
+        <p>3 years managing servers, networks and infrastructure, <br/> 3 years as a front-end developer and 2 years of internship in systems analysis.</p>
       </div>
       <div className="experience-skills">
         <div className="experience-time">
           <Skill image="/logo-react.png" measure={80} years="2 years" />
           <Skill image="/logo-typescript.png" measure={60} years="3 years" />
-          <Skill image="/logo-tailwind.png" measure={40} years="3 years" />
+          <Skill image="/logo-tailwind.png" measure={60} years="3 years" />
         </div>
         <div className="experience-time">
           <Skill image="/logo-java.png" measure={50} years="2 years" />
-          <Skill image="/logo-node.png" measure={20} years="3 years" />
+          <Skill image="/logo-node.png" measure={80} years="3 years" />
           <Skill image="/logo-bd.png" measure={50} years="3 years" />
         </div>
         <div className="experience-time">
-        <Skill image="/logo-docker.png" measure={35} years="2 years" />
+        <Skill image="/logo-docker.png" measure={50} years="2 years" />
         <Skill image="/logo-git.png" measure={80} years="3 years" />
-        <Skill image="/logo-linux.png" measure={60} years="3 years" />
+        <Skill image="/logo-linux.png" measure={70} years="3 years" />
         </div>
       </div>
     </div>
